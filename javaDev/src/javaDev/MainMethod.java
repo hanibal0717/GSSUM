f94949b4242c7c1a9e48ed89529cd907d7ee6212
hanibal <hanibal0717@gmail.com>
2017-11-14 14:25:03 +0900

    third line add

--- javaDev/src/javaDev/MainMethod.java
+++ javaDev/src/javaDev/MainMethod.java
@@ -4,5 +4,6 @@ public class MainMethod {
 	public static void main(String[] args){
 		System.out.println("hanibal dev open");
 		System.out.println("first command");
+		System.out.println("2017년11월14");
 	}
 }
